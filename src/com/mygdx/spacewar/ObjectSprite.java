1d23d53990b992997d9e99cca1f261bd91c5a7eb
kpodranyuk <katie.katie2011@yandex.ru>
2017-10-19 13:41:24 +0300

    Changed ObjectSprite

--- src/com/mygdx/spacewar/ObjectSprite.java
+++ src/com/mygdx/spacewar/ObjectSprite.java
@@ -15,7 +15,7 @@ import com.badlogic.gdx.math.Rectangle;
 public class ObjectSprite {
     private Texture texture;    /// Текстура 
     public Rectangle rect;     /// Прямоугольник отображения
-
+    private int id;
     /**
      * Возможные типы объекта
      */
@@ -28,9 +28,10 @@ public class ObjectSprite {
      * @param width Ширина объекта на игровом поле
      * @param height Высота объекта на игровом поле
      * @param type Тип объекта
+     * @param id Идентификатор объекта
      */
-    public ObjectSprite(String img, int width, int height, ObjectType type){
-        if (img == null || img.equals("") || width <= 0 || height <= 0 || type == null)
+    public ObjectSprite(String img, int width, int height, ObjectType type, int id){
+        if (img == null || img.equals("") || width <= 0 || height <= 0 || type == null || id<0)
             throw new Error("Can't create view of object");
         this.texture = new Texture(img);
         this.rect = new Rectangle();
@@ -41,19 +42,20 @@ public class ObjectSprite {
         this.rect.y = 0;
         
         this.type = type;
+        this.id = id;
     }
     
     /**
      * Конструктор копирования
      * @param other Другое отображение
      */
-    public ObjectSprite(ObjectSprite other){
+    /*public ObjectSprite(ObjectSprite other){
         if (other == null)
             throw new Error("Can't create copy of null ObjectSprite");
         this.rect = other.getRectangle();
         this.texture = other.getTexture();
         this.type = other.getObjType();
-    }
+    }*/
     
     /**
      * Получить текстуру объекта
@@ -77,5 +79,13 @@ public class ObjectSprite {
      */
     public ObjectType getObjType(){
         return this.type;
+    }
+    
+    /**
+     * Получить Id объекта
+     * @return Id объекта
+     */
+    public int getId(){
+        return this.id;
     }
 }
