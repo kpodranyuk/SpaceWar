a9119640373eca57f824fd4e914342493b746d03
kpodranyuk <katie.katie2011@yandex.ru>
2018-01-14 17:41:50 +0300

    Added realization for fast missiles.

--- src/com/mygdx/spacewar/EnemyMissileBuilder.java
+++ src/com/mygdx/spacewar/EnemyMissileBuilder.java
@@ -7,6 +7,7 @@ package com.mygdx.spacewar;
 
 import com.badlogic.gdx.utils.Array;
 import static com.mygdx.spacewar.ObjectImage.ObjectType.ENMMISSILE;
+import static com.mygdx.spacewar.ObjectImage.ObjectType.ENMMISSILEFAST;
 
 /**
  * Строитель снарядов врагов
@@ -21,11 +22,13 @@ public class EnemyMissileBuilder {
     
     public Array<Missile> generateEnemyMissiles(ObjectImage.ObjectType missileType){
         if(missileType==ENMMISSILE)
-            return geneateDefaultMissile();
+            return geneateDefaultMissiles();
+        if(missileType==ENMMISSILEFAST)
+            return generateFastMissiles();
         return null;
     }
     
-    private Array<Missile> geneateDefaultMissile(){
+    private Array<Missile> geneateDefaultMissiles(){
         ObjectSprite enemiesMissileView = new ObjectSprite(enemiesMissileSprite, 22, 11, ++(GameSystem.curId));        
         // Снаряд легкого врага летит по прямой траектории
         StraightTrajectory enemiesTrajectory = new StraightTrajectory((float) 250.0, true);
@@ -35,5 +38,30 @@ public class EnemyMissileBuilder {
         Array<Missile> ms = new Array();
         ms.add(enemiesMissile);
         return ms;        
+    }
+    
+    private Array<Missile> generateFastMissiles(){
+        ObjectSprite enemiesMissileView = new ObjectSprite(enemiesMissileSprite, 22, 11, ++(GameSystem.curId));
+        Array<Missile> mssls = new Array<Missile>();
+        // Снаряд данного врага стреляет по дуге, поэтому у него дуговая траектория
+        ArcTrajectory enemiesTrajectory = new ArcTrajectory((float) 330.0, true, true);
+        // Создаем снаряд врага
+        Missile enemiesMissile = new Missile(2, (float) 330.0, enemiesTrajectory, enemiesMissileView); 
+        mssls.add(enemiesMissile);
+        
+        enemiesMissileView = new ObjectSprite(enemiesMissileSprite, 22, 11, ++(GameSystem.curId));
+        // Снаряд данного врага стреляет по дуге, поэтому у него дуговая траектория
+        StraightTrajectory enemiesSTrajectory = new StraightTrajectory((float) 330.0, true);
+        // Создаем снаряд врага
+        enemiesMissile = new Missile(2, (float) 330.0, enemiesSTrajectory, enemiesMissileView); 
+        mssls.add(enemiesMissile);
+        
+        enemiesMissileView = new ObjectSprite(enemiesMissileSprite, 22, 11, ++(GameSystem.curId));
+        // Снаряд данного врага стреляет по дуге, поэтому у него дуговая траектория
+        enemiesTrajectory = new ArcTrajectory((float) 330.0, true, false);
+        // Создаем снаряд врага
+        enemiesMissile = new Missile(2, (float) 330.0, enemiesTrajectory, enemiesMissileView); 
+        mssls.add(enemiesMissile);
+        return mssls;
     }
 }
