d6130836478406fa6c6fdfe3a9b825609fe63756
kpodranyuk <katie.katie2011@yandex.ru>
2018-01-14 17:46:57 +0300

    Added realization for EnemyBuilder methods.
    Now whole default enemies are generated via Builder.

--- src/com/mygdx/spacewar/EnemyBuilder.java
+++ src/com/mygdx/spacewar/EnemyBuilder.java
@@ -7,7 +7,9 @@ package com.mygdx.spacewar;
 
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
+import com.mygdx.spacewar.ObjectImage.ObjectType;
 import static com.mygdx.spacewar.ObjectImage.ObjectType.ENMMISSILE;
+import static com.mygdx.spacewar.ObjectImage.ObjectType.ENMMISSILEFAST;
 import static com.mygdx.spacewar.ObjectImage.ObjectType.ENMSHIP;
 import static com.mygdx.spacewar.ObjectImage.ObjectType.ENMSHIPFAST;
 import static com.mygdx.spacewar.ObjectImage.ObjectType.ENMSHIPHEALTHY;
@@ -46,11 +48,11 @@ public class EnemyBuilder {
             return generateEasyEnemies(1);
         }
         // Иначе если второй тип, то создаем врага с большим хп
-        else if(enemyIndex==2){
+        if(enemyIndex==2){
             return generateHealthyEnemies(1);//generateHealthyEnemy();
         }
         // Иначе если третий тип, то создаем врага, стреляющего по дуге
-        else if(enemyIndex==3){
+        if(enemyIndex==3){
             return generateFastEnemies(1);//generateFastEnemy();
         }            
         return null;
@@ -69,7 +71,7 @@ public class EnemyBuilder {
             // Создаем отображение врага
             ObjectSprite enemiesView = null;
             // Получаем изображение врага
-            ObjectImage enemiesImg = enemiesSprites.first();        
+            ObjectImage enemiesImg = getImageOfEnemyObject(ENMSHIP);        
             // Создаем спрайт врага
             enemiesView = new ObjectSprite(enemiesImg, 29, 38, ++(GameSystem.curId));
 
@@ -93,8 +95,24 @@ public class EnemyBuilder {
      * @return Враги
      */
     private Array<EnemyShip> generateHealthyEnemies(int count){
-        // Заглушка
-        return null;
+        if (count<=0)
+            return null;
+        Array<EnemyShip> enms = new Array();
+        while(enms.size<count){
+            // Создаем отображение врага
+            ObjectSprite enemiesView = null;
+            // Запрашиваем имеется ли в памяти изображение врага
+            ObjectImage enemiesImg = getImageOfEnemyObject(ENMSHIPHEALTHY);
+            // Создаем спрайт врага
+            enemiesView = new ObjectSprite(enemiesImg, 40, 52, ++(GameSystem.curId));
+            // Создаем врага с учетом того, что у данного типа нет снарядов и большое хп
+            Array<Weapon> wps = new Array<Weapon>();
+            wps.add(new Weapon(null));
+            EnemyShip enemy = new EnemyShip(6, (float) 170.0, enemiesView, wps);
+            enms.add(enemy);
+        }
+        // Возвращаем созданного врага
+        return enms;
     }
     
     /**
@@ -103,7 +121,34 @@ public class EnemyBuilder {
      * @return Враги
      */
     private Array<EnemyShip> generateFastEnemies(int count){
-        // Заглушка
+        if (count<=0)
+            return null;
+        Array<EnemyShip> enms = new Array();
+        while(enms.size<count){
+            // Создаем отображение врага
+            ObjectSprite enemiesView = null;
+            // Запрашиваем в памяти изображение врага
+            ObjectImage enemiesImg = getImageOfEnemyObject(ENMSHIPFAST);
+            // Создаем спрайт врага
+            enemiesView = new ObjectSprite(enemiesImg, 19, 31, ++(GameSystem.curId));      
+            Array<Missile> enemyMissiles = this.missileBuilder.generateEnemyMissiles(ENMMISSILEFAST);
+            Array<Weapon> wps = new Array<Weapon>();
+            for(Missile em: enemyMissiles){
+                wps.add(new Weapon(em));
+            }        
+            // Создаем врага
+            EnemyShip enemy = new EnemyShip(2, (float) 300.0, enemiesView, wps);
+            enms.add(enemy);
+        }
+        // Возвращаем созданного врага
+        return enms;
+    }
+    
+    private ObjectImage getImageOfEnemyObject(ObjectType type){
+        for(ObjectImage img: this.enemiesSprites){
+            if(img.getObjType()==type)
+                return img;
+        }
         return null;
     }
 }
